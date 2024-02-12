154b4bdcde7aab9556e2b081ae58f121bb8b24fe
Cursed Entertainment <cursedsoft@gmail.com>
2024-02-12 23:20:18 +0200

    ...

--- scripts/system/tts.py
+++ scripts/system/tts.py
@@ -3,6 +3,7 @@ import subprocess
 from gtts import gTTS
 import speech_recognition as sr
 from .clean_text import clean_text
+from .play_audio import play_audio
 
 def text_to_speech(text):
     cleaned_text = clean_text(text)
@@ -12,9 +13,5 @@ def text_to_speech(text):
     os.makedirs(os.path.dirname(audio_path), exist_ok=True)
 
     tts.save(audio_path)
-
-    # Open the audio file with the default media player
-    try:
-        subprocess.run(["start", audio_path], check=True, shell=True)
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
+    tts.save(audio_path)
+    play_audio(audio_path)
